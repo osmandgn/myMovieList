006e96a158fcc28f08aa234765a63c05ea3f2a02
osmandgn <oskan001@gmail.com>
2023-03-07 12:34:53 +0300

    V1

---
+++ src/test/java/org/filmlist/FilmlistApplicationTests.java
@@ -0,0 +1,13 @@
+package org.filmlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
