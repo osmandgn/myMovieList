860b3dd689c9ec646de2daa22025abeff280df85
osmandgn <oskan001@gmail.com>
2023-03-07 13:10:16 +0300

    V1

---
+++ src/main/java/org/filmlist/repository/ContactMessageRepository.java
@@ -0,0 +1,9 @@
+package org.filmlist.repository;
+
+import org.filmlist.domain.ContactMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
+}
---
+++ src/main/java/org/filmlist/service/ContactMessageService.java
@@ -0,0 +1,7 @@
+package org.filmlist.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactMessageService {
+}
