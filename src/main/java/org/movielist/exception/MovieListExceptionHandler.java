ef5c3d2d5a6e1f346dc1d4697d1d090e2f6df437
osmandgn <oskan001@gmail.com>
2023-03-09 00:03:48 +0300

    V3

--- src/main/java/org/movielist/exception/MovieListExceptionHandler.java
+++ src/main/java/org/movielist/exception/MovieListExceptionHandler.java
@@ -1,6 +1,30 @@
 package org.movielist.exception;
 
 
-public class MovieListExceptionHandler extends JpaE{
+import org.movielist.exception.message.ApiResponseError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class MovieListExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private ResponseEntity<Object> buildResponseEntity(ApiResponseError error){
+        return new ResponseEntity<>(error, error.getStatus());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    protected ResponseEntity<Object> handleResourceNotFoundException(
+            ResourceNotFoundException ex, WebRequest request){
+        ApiResponseError error = new ApiResponseError(HttpStatus.NOT_FOUND,
+                                                      ex.getMessage(),
+                                                      request.getDescription(false));
+        return buildResponseEntity(error);
+
+    }
+
 
 }
