b0733920b5a90f30e16cd20daaedda54d0409706
osmandgn <oskan001@gmail.com>
2023-03-09 12:48:18 +0300

    V3

--- src/main/java/org/movielist/exception/MovieListExceptionHandler.java
+++ src/main/java/org/movielist/exception/MovieListExceptionHandler.java
@@ -2,12 +2,17 @@ package org.movielist.exception;
 
 
 import org.movielist.exception.message.ApiResponseError;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class MovieListExceptionHandler extends ResponseEntityExceptionHandler {
@@ -26,5 +31,45 @@ public class MovieListExceptionHandler extends ResponseEntityExceptionHandler {
 
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> erros = ex.getBindingResult().getFieldErrors()
+                .stream().
+                map(e -> e.getDefaultMessage()).
+                collect(Collectors.toList());
+
+        ApiResponseError error = new ApiResponseError(
+                HttpStatus.BAD_REQUEST,
+                erros.get(0).toString(),
+                request.getDescription(false));
+
+        return buildResponseEntity(error);
+    }
+
+
+
+
+
+
+
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<Object> handleRuntimeException(RuntimeException ex, WebRequest request) {
+        ApiResponseError error = new ApiResponseError(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                request.getDescription(false));
+        return buildResponseEntity(error);
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<Object> handleGeneralException(Exception ex, WebRequest request){
+        ApiResponseError error = new ApiResponseError(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                request.getDescription(false));
+        return buildResponseEntity(error);
+    }
 
 }
