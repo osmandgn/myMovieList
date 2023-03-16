97b2b8ecb26cd332e6ab2a73d64f42593bfb75bb
osmandgn <oskan001@gmail.com>
2023-03-16 15:54:43 +0300

    V3

---
+++ src/main/java/org/movielist/domain/Deme.java
@@ -0,0 +1,13 @@
+package org.movielist.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Deme {
+    Long id;
+    String name;
+    String surname;
+    String puan;
+}
