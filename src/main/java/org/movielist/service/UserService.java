4375fe5911b2e9c63777c734202b809c0af9ed85
osmandgn <oskan001@gmail.com>
2023-04-13 22:20:14 +0300

    V4

--- src/main/java/org/movielist/service/UserService.java
+++ src/main/java/org/movielist/service/UserService.java
@@ -1,4 +1,23 @@
 package org.movielist.service;
 
+import org.movielist.domain.User;
+import org.movielist.exception.ResourceNotFoundException;
+import org.movielist.exception.message.ErrorMessage;
+import org.movielist.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserByEmail(String email){
+       return userRepository.findByEmail(email).orElseThrow(
+                ()-> new ResourceNotFoundException(
+                        String.format(ErrorMessage.USER_NOT_FOUND_EXCEPTION, email)));
+    }
 }
