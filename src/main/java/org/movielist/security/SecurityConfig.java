aae9f6eefa2cd87920781f15f517789108f40a02
osmandgn <oskan001@gmail.com>
2023-04-15 14:22:31 +0300

    V3

--- src/main/java/org/movielist/security/SecurityConfig.java
+++ src/main/java/org/movielist/security/SecurityConfig.java
@@ -1,5 +1,30 @@
 package org.movielist.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().
+                authorizeRequests()
+                .antMatchers("/", "register", "login", "index.html").permitAll()
+                .anyRequest().authenticated();
+    }
 }
