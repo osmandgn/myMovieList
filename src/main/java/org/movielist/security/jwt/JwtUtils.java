cec6d73b03d89f3e65955ac57ccc739c79f2d7f5
osmandgn <oskan001@gmail.com>
2023-04-17 17:01:00 +0300

    V3

--- src/main/java/org/movielist/security/jwt/JwtUtils.java
+++ src/main/java/org/movielist/security/jwt/JwtUtils.java
@@ -1,10 +1,32 @@
 package org.movielist.security.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${filmlist.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${filmlist.app.jwtExpirationMs}")
+    private Long jwtExpirationMs;
+
+
+    public String generateJwtToken(UserDetails userDetails){
+       return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.ES512, jwtSecret)
+                .compact();
+    }
 }
