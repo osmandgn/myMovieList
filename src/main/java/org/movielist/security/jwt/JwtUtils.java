a22ab640574adaa80a6a23fb2124513376120b2d
osmandgn <oskan001@gmail.com>
2023-04-17 16:04:37 +0300

    V3

---
+++ src/main/java/org/movielist/security/jwt/JwtUtils.java
@@ -0,0 +1,10 @@
+package org.movielist.security.jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+}
