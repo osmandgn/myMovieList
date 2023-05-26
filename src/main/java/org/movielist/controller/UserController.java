de23199cd8a0e02357c9843193447ea74b4974a6
osmandgn <oskan001@gmail.com>
2023-05-26 16:10:22 +0300

    V5

--- src/main/java/org/movielist/controller/UserController.java
+++ src/main/java/org/movielist/controller/UserController.java
@@ -3,10 +3,15 @@ package org.movielist.controller;
 import org.movielist.domain.User;
 import org.movielist.dto.UserDTO;
 import org.movielist.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -34,6 +39,21 @@ public class UserController {
     public ResponseEntity<UserDTO> getUser(){
         UserDTO userDTO = userService.getPrincipal();
         return ResponseEntity.ok(userDTO);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Page<UserDTO>> getAllUsersByPage(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sort") String prop,
+            @RequestParam(value="direction",
+                            required = false,
+                            defaultValue = "DESC") Sort.Direction direction){
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, prop));
+        Page<UserDTO> userDTOPage = userService.getUserPage(pageable);
+        return ResponseEntity.ok(userDTOPage);
+
     }
 
 
--- src/main/java/org/movielist/repository/UserRepository.java
+++ src/main/java/org/movielist/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package org.movielist.repository;
 
 import org.movielist.domain.User;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
