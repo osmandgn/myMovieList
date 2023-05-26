9cda2d2f346ec339aa0c0f8173a3eb6cdf7deaef
osmandgn <oskan001@gmail.com>
2023-05-27 02:12:16 +0300

    V5

--- src/main/java/org/movielist/repository/UserRepository.java
+++ src/main/java/org/movielist/repository/UserRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -26,4 +29,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @EntityGraph(attributePaths = "roles")
     Optional<User> findById(Long id);
+
+    @Modifying
+    @Query("UPDATE User u SET u.firstName=:firstName, u.lastName=:lastName,u.phoneNumber=:phoneNumber,u.email=:email,u.address=:address,u.zipCode=:zipCode WHERE u.id=:id")
+    void update(@Param("id") Long id,
+                @Param("firstName") String firstName,
+                @Param("lastName") String lastName,
+                @Param("phoneNumber") String phoneNumber,
+                @Param("email") String email,
+                @Param("address") String address,
+                @Param("zipCode") String zipCode);
+
 }
